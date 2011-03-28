db4c7ca1317b13fa5e34e52ce0b8e2bc7dc0aee4
simone6192 <simone6192@b2e542df-5cc8-4ffb-8fee-aa8f5de9161d>
2011-03-28 21:59:35 +0000

    Spanish tray translation update
    git-svn-id: https://svn.code.sf.net/p/appset/code@55 b2e542df-5cc8-4ffb-8fee-aa8f5de9161d

---
+++ Qt/AppSetTray-Qt/appsettray-qt_es.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="trayicon.cpp" line="23"/>
+        <source>Launch AppSet-Qt</source>
+        <translation>Iniciar AppSet Qt</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="24"/>
+        <source>Check for updates NOW!</source>
+        <translation>Buscar actualizaciones</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="25"/>
+        <source>Quit Tray</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="59"/>
+        <source>Waiting helper...</source>
+        <translation>Esperando al asistente...</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="72"/>
+        <source>Are you sure to quit this tray?</source>
+        <translation>¿Estás seguro de salir?</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="73"/>
+        <source>You can restart it from desktop menu</source>
+        <translation>Puedes reiniciar el programa del menú del escritorio</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="93"/>
+        <source>AppSet-Qt is Running!</source>
+        <translation>¡AppSet Qt está en marcha!</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="116"/>
+        <source>There are updates for:</source>
+        <translation>Hay actualizaciones para:</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="116"/>
+        <source>There is an update for:</source>
+        <translation>Hay una actualización para:</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="126"/>
+        <source>
+And others...</source>
+        <translation>
+Y otros...</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="129"/>
+        <location filename="trayicon.cpp" line="131"/>
+        <source> updates available!</source>
+        <translation> actualizaciones disponibles</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="129"/>
+        <location filename="trayicon.cpp" line="131"/>
+        <source> update available!</source>
+        <translation> actualización disponible</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="135"/>
+        <location filename="trayicon.cpp" line="138"/>
+        <source>No updates available</source>
+        <translation>No hay actualizaciones disponibles</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="162"/>
+        <source>Launching AppSet-Qt</source>
+        <translation>Iniciando AppSet Qt</translation>
+    </message>
+    <message>
+        <location filename="trayicon.cpp" line="162"/>
+        <source>Wait...</source>
+        <translation>Espere...</translation>
+    </message>
+</context>
+</TS>
